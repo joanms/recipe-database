ba0164946e058c245f262009a3b66be38d80ce39
Joan Sweeney <joanms@eircom.net>
2018-12-08 14:24:54 +0000

    Added app.py with route to recipes

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipedb'
+app.config["MONGO_URI"] = 'mongodb://admin:o1deA$@ds127624.mlab.com:27624/recipedb'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/show_recipe')
+def show_recipe():
+    return render_template("recipe.html", 
+    recipes=mongo.db.recipes.find())
