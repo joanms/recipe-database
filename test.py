8af9814abba1141d2f5bc1a084ea4fc2b0f221ee
Joan Sweeney <joanms@eircom.net>
2019-01-05 15:23:48 +0000

    Add unittest

---
+++ test.py
@@ -0,0 +1,14 @@
+# The tests are based on this video: https://www.youtube.com/watch?v=1aHNs1aEATg&list=PLLjmbh6XPGK4ISY747FUHXEl9lBxre4mM&index=8&t=0s
+from app import app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+
+          # Ensure that Flask was set up correctly
+          def test_index(self):
+                    tester = app.test_client(self)
+                    response = tester.get('/', content_type='html/text')
+                    self.assertEqual(response.status_code, 200)
+          
+if  __name__ == '__main__':
+          unittest.main()
