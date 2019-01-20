34d7e4eb699921c2f2e59631e97de1dc16dfb632
Joan Sweeney <joanms@eircom.net>
2019-01-20 12:47:12 +0000

    Add notes to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,16 @@
+// This initialises the side-nav for small screens
 $(document).ready(function() {
     $('.collapsible').collapsible();
     $('select').material_select();
     $(".button-collapse").sideNav();
 });
             
-$(document).ready(function(){
-    $('select').formSelect();
-});
+// This initialises the dropdown menus in the forms
+$(document).ready(function() {
+    $('select').material_select();
+  });
 
+// This ensures that the text ares in the form resize automatically when they have dynamically generated content
 $(document).ready(function(){
     $('.materialize-textarea').trigger('autoresize');
 });
