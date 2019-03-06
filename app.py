b9b4f91339eb8255f202af927f1fe8473f02251d
Joan Sweeney <joanms@eircom.net>
2019-03-06 18:53:36 +0000

    Change notes in app.py to docstrings

--- app.py
+++ app.py
@@ -21,22 +21,31 @@ recipes =  mongo.db.recipes
 mongo.db.recipes.create_index([('$**', 'text')])
 mongo.db.users.create_index([('$**', 'text')])
 
-# Home page with links to recipe categories
+
 @app.route('/')
 def index():
+    
+    """Home page with links to recipe categories"""
+    
     return render_template('index.html', 
         allergens=mongo.db.allergens.find())
 
-# Form to add a new recipe to the database
+
 @app.route('/add_recipe')
 def add_recipe():
+    
+    """Form to add a new recipe to the database"""
+    
     return render_template(
         'add_recipe.html', categories=mongo.db.categories.find(), 
         allergens=mongo.db.allergens.find())
 
-# Post a new recipe to the database after filling in the form
+
 @app.route('/insert_recipe', methods=['GET', 'POST'])
 def insert_recipe():
+    
+    """Post a new recipe to the database after filling in the form"""
+    
     recipes =  mongo.db.recipes
     new_recipe = {
         'recipe_title': request.form.get('recipe_title'),
@@ -59,18 +68,24 @@ def insert_recipe():
     return render_template('list_recipes.html', 
     recipes=recipes.find().sort('_id',-1)) # The recipe list will load with the new recipe at the top
     
-# Form to edit a recipe
+
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
+    
+    """Form to edit a recipe"""
+    
     the_recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
     all_categories = mongo.db.categories.find()
     allergens=mongo.db.allergens.find()
     return render_template(
         'edit_recipe.html', recipe=the_recipe, categories=all_categories, allergens=allergens)
         
-# Submit changes after editing
+
 @app.route('/submit_changes/<recipe_id>', methods=['POST'])
 def submit_changes(recipe_id):
+    
+    """Submit changes after editing"""
+    
     mongo.db.recipes.update(
         {'_id': ObjectId(recipe_id)},
         {
@@ -94,79 +109,110 @@ def submit_changes(recipe_id):
     return render_template(
         'show_recipe.html', recipe=the_recipe)
     
-# Search for a recipe by keywords
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
+    
+    """Search for a recipe by keywords"""
+    
     keywords = request.form.get('search')
     query = ( { '$text': { '$search': keywords } } )
     results = mongo.db.recipes.find(query)
     return render_template('list_recipes.html', 
     recipes=results, count=results.count())
     
-# Search for vegetarian recipes
+
 @app.route('/vegetarian', methods=['GET', 'POST'])
 def vegetarian():
+    
+    """Search for vegetarian recipes"""
+    
     query = ( { 'vegetarian': 'on' } )
     results = mongo.db.recipes.find(query)
     return render_template('list_recipes.html', 
     recipes=results, count=results.count())
     
-# Search for vegan recipes
+
 @app.route('/vegan', methods=['GET', 'POST'])
 def vegan():
+    
+    """Search for vegan recipes"""
+    
     query = ( { 'vegan': 'on' } )
     results = mongo.db.recipes.find(query)
     return render_template('list_recipes.html', 
     recipes=results, count=results.count())
 
-# Search for gluten-free recipes
+
 @app.route('/gluten_free', methods=['GET', 'POST'])
 def gluten_free():
+    
+    """Search for gluten-free recipes"""
+
     query = ( { 'gluten_free': 'on' } )
     results = mongo.db.recipes.find(query)
     return render_template('list_recipes.html', 
     recipes=results, count=results.count())
 
-# Search for recipes without selected allergens
+
 @app.route('/allergens', methods=['GET', 'POST'])
 def allergens():
+    
+    """Search for recipes without selected allergens"""
+
     allergens = request.form.getlist('allergens')
     query = ( { 'allergens': { '$nin': allergens} } )
     results = mongo.db.recipes.find(query)
     return render_template('list_recipes.html', 
     recipes=results, count=results.count())
     
-# List either all recipes in the database or search results
+
 @app.route('/list_recipes', methods=['GET', 'POST'])
 def list_recipes():
+    
+    """List either all recipes in the database or search results"""
+
     return render_template('list_recipes.html', 
     recipes=recipes.find().sort('recipe_title',1), count=recipes.count())
     
-# List recipes by category
+
 @app.route('/list_recipes_by_cat/<category_name>', methods=['GET', 'POST'])
 def list_recipes_by_cat(category_name):
+    
+    """List recipes by category"""
+
     results=recipes.find({'category_name': category_name}).sort('recipe_title',1)
     return render_template('list_recipes.html', 
     recipes=results, count=results.count())
 
-# Display a recipe, including ingredients, method and other information such as suitability for restricted diets
+
 @app.route('/show_recipe/<recipe_id>', methods=['GET', 'POST'])
 def show_recipe(recipe_id):
+    
+    """Display a recipe, including ingredients, method and other information 
+    such as suitability for restricted diets
+    """
+
     the_recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
     return render_template(
         'show_recipe.html', recipe=the_recipe)
 
-# A warning dipslays when a user clicks the Delete button
 @app.route('/warning/<recipe_id>', methods=['GET', 'POST'])
 def warning(recipe_id):
+    
+    """A warning dipslays when a user clicks the Delete button"""
+
     the_recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
     flash('This will permanently delete the recipe. Are you sure?')
     return render_template(
         'show_recipe.html', recipe=the_recipe)
 
-# The recipe is deleted when the user confirms the deletion
+
 @app.route('/delete_recipe/<recipe_id>', methods=['GET', 'POST'])
 def delete_recipe(recipe_id):
+    
+    """The recipe is deleted when the user confirms the deletion"""
+
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('list_recipes'))
 
