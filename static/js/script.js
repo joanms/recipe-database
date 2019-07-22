b8a2e7788391d023cf01852a43f026cf55989da7
Joan Sweeney <joanms@eircom.net>
2019-07-22 11:01:58 +0000

    Remove repetition from script.js

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,10 @@ $(document).ready(function() {
     $('.collapsible').collapsible();
     $('select').material_select();
     $(".button-collapse").sideNav();
-});
             
 // This initialises the dropdown menus in the forms
-$(document).ready(function() {
     $('select').material_select();
-  });
 
 // This ensures that the text ares in the form resize automatically when they have dynamically generated content
-$(document).ready(function(){
     $('.materialize-textarea').trigger('autoresize');
 });
